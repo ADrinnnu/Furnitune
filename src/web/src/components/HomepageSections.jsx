285142d31f8ad96904be5e7947f596e17d4e52a4
Waffle-3 <rjaysantos611@gmail.com>
2025-09-01 00:05:40 +0800

    updated Footer and added image to homepagesection

--- src/web/src/components/HomepageSections.jsx
+++ src/web/src/components/HomepageSections.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import cpnImg from "../assets/cpn.jpeg";
+import cusImg from "../assets/cust.jpg"
+import recImg from "../assets/recc.jpg"
+import desImg from "../assets/des.jpg"
+import qualImg from "../assets/qual.jpg"
+import delImg from "../assets/deliv.webp"
+import cussImg from "../assets/cuss.jpg"
+
 
 const features = [
   {
     title: "VISIT US!",
     description:
       "Visit our store and explore our collection in a space that sparks inspiration. Feel free to visit our physical store.",
-    button: "Click to view location",
-    image: "/images/map.png",
+    button: "Click to view our physical store location",
+    image: cpnImg,
     path: "/VisitUs" // 👈 route for this button
   },
   {
@@ -15,15 +23,15 @@ const features = [
     description:
       "Bring your vision to life with our customization feature—make your own piece in your own way.",
     button: "Click to start customizing your furniture",
-    image: "/images/customize.png",
+    image: cusImg,
     path: "/Customization" 
   },
   {
     title: "ASK FURNITURE SUGGESTION!",
     description:
       "Discover furniture that fits your style with our smart recommendation system.",
-    button: "Take a short quiz to get a suggestion",
-    image: "/images/recommendation.png",
+    button: "Take a short quiz to get a suggestions",
+    image: recImg,
     path: "" 
   }
 ];
@@ -34,28 +42,28 @@ const reviews = [
     quote:
       "Solid. Maayos-kalfa ang gandang-ganda ng design that fits my room. Very modern, subtle na warm design na perfectly nadala nila.",
     name: "Austria, JC | San Miguel, Tarlac",
-    image: "/images/review-design.png"
+    image: desImg
   },
   {
     title: "QUALITY",
     quote:
       "The quality is so good. I love their furniture so much. Every time the quality, the materials, every piece is a masterpiece.",
     name: "Santos, Rhey | Concepcion, Tarlac",
-    image: "/images/review-quality.png"
+    image: qualImg
   },
   {
     title: "DELIVERY",
     quote:
       "The delivery is on time and under bills. Even with a big package, men, bali-bali and energetic sila. Paulyn service talaga and that’s what makes it good!",
     name: "Valencia, Enrica | Malacampa, Tarlac City",
-    image: "/images/review-delivery.png"
+    image: delImg
   },
   {
     title: "CUSTOMER SERVICE",
     quote:
-      "Ang bilis ng reply nila and super bait nila sa queries. May guiding assistant, chatbot sa recommendation system. Nakakatulong siya so I find a furniture that fits sa taste ko.",
+      "Ang bilis ng reply nila and super bait nila sa queries. May guiding assistant, chatbot sa recommendation system. Nakakatulong siya .",
     name: "Villanueva, Aldrin | Paniqui, Tarlac",
-    image: "/images/review-customer.png"
+    image: cussImg
   }
 ];
 
