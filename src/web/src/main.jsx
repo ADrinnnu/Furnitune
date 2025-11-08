44a0cc1a897b64aaceecc7afa7f2f949509795c2
Aldrin R. Villanueva <aldrinvillanueva139@gmail.com>
2025-11-08 21:34:00 +0800

    update

--- src/web/src/main.jsx
+++ src/web/src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Furnitune">
   <CartProvider>
     <App />
     </CartProvider>
